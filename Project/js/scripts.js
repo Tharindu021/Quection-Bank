89502bffcfddd1e053c17eab4b5bcc89ff194eed
Tharindu021 <111674129+Tharindu021@users.noreply.github.com>
2022-08-22 08:41:59 +0530

    add project files

---
+++ Project/js/scripts.js
@@ -0,0 +1,6 @@
+function clearAnswers() {
+    document.getElementById('answer1').checked = false;
+    document.getElementById('answer2').checked = false;
+    document.getElementById('answer3').checked = false;
+    document.getElementById('answer4').checked = false;
+}
